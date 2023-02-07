b5450b61eebfdc1ff037e77543a05190f1d58d67
DarrylWilliams32 <77248777+DarrylWilliams32@users.noreply.github.com>
2023-02-07 11:10:32 -0500

    initial commit

---
+++ Client/src/App.js
@@ -0,0 +1,11 @@
+import ListHeader from './components/ListHeader'
+
+const App = () => {
+  return (
+    <div className="app">
+      <ListHeader listName={'🎩 Boss Check List'} />
+    </div>
+  )
+}
+
+export default App;
